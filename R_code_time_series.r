bba2757289890e12be6c9fc721c2fc9599d954ff
guiaburbui <80713315+guiaburbui@users.noreply.github.com>
2021-04-02 12:56:19 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,9 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from Emanuela Cosma
+
+# install.packages("raster")
+library(raster)
+
+# Impostazione della directory di lavoro
+setwd("C:/lab/greenland")
